79159d409988801471f5b5f54f06eb543b2095e8
AntonelaDuckwen <antoduckwen@gmail.com>
2024-01-21 20:11:52 -0300

    subo cambios

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/public/css/general.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/css/general.css'))
+})
+
 
 app.listen(port,() => console.log(`http://localhost:${port}`))
